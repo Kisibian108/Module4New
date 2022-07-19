7400697608dfa3c799677ebabe3c46238ce9135c
Kisibian108 <102302311+Kisibian108@users.noreply.github.com>
2022-07-19 07:42:32 +0700

    to khai y te

--- ss4_DataBinding/Exercise/HealthDeclaration/src/main/java/com/codegym/controller/Medical.java
+++ ss4_DataBinding/Exercise/HealthDeclaration/src/main/java/com/codegym/controller/Medical.java
@@ -3,12 +3,10 @@ package com.codegym.controller;
 import com.codegym.model.MedicalDeclaration;
 import com.codegym.service.IMedicalService;
 import com.codegym.service.MedicalServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
